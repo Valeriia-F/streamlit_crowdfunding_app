3bf6283e86e91889a4e7df69f0060a3ae90b9f79
Valeriia Fedorova <leraf11551@gmail.com>
2025-09-21 14:15:49 +0500

    Changes

--- app_crowdfunding.py
+++ app_crowdfunding.py
@@ -426,23 +426,30 @@ elif st.session_state.page == "results":
                      'Music': {'Others': 52.06, 'USA': 58.45}, 'Photography': {'Others': 35.71, 'USA': 33.9},
                      'Publishing': {'Others': 57.6, 'USA': 53.82}, 'Technology': {'Others': 28.86, 'USA': 34.7},
                      'Theater': {'Others': 58.51, 'USA': 49.61}}
+    
+    # Форматируем число и применяем стили через HTML
+    formatted_probability = f"{st.session_state.predicted_proba * 100:.2f}%"
 
-    st.markdown('<div style="margin-top: 120px;"></div>', unsafe_allow_html=True)
-    st.title(round(st.session_state.predicted_proba * 100, 2))
+    # Используем st.markdown с HTML и CSS для стилизации
+    st.markdown(
+        f'<div style="font-size: 300px; font-weight: bold; color: #17D989;">{formatted_probability}</div>',
+        unsafe_allow_html=True
+    )
 
     if st.session_state.predicted_proba * 100 > success_rates[st.session_state.category][st.session_state.country]:
-        st.write(f"% the probability of success of your project. "
+        st.write(f"the probability of success of your project. "
                  f"This is higher than for similar projects "
                  f"({success_rates[st.session_state.category][st.session_state.country]}%).")
     elif st.session_state.predicted_proba * 100 < success_rates[st.session_state.category][st.session_state.country]:
-        st.write(f"% the probability of success of your project. "
+        st.write(f"the probability of success of your project. "
                  f"This is lower than for similar projects "
                  f"({success_rates[st.session_state.category][st.session_state.country]}%).")
     else:
-        st.write(f"% the probability of success of your project. "
+        st.write(f"the probability of success of your project. "
                  f"This is the same as for similar projects "
                  f"({success_rates[st.session_state.category][st.session_state.country]}%).")
 
+    st.markdown('<div style="margin-top: 120px;"></div>', unsafe_allow_html=True)
 
     st.header("Recommendations")
 
